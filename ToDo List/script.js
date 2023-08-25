a69c564bec56274186f34c5271533031d1c69a1d
Ghulam Ammar Yanuar <65816301+ghulam46@users.noreply.github.com>
2023-08-25 10:22:57 +0700

    update progress

--- ToDo List/script.js
+++ ToDo List/script.js
@@ -9,6 +9,17 @@ function addTask() {
         const li = document.createElement('li');
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
+        const span = document.createElement('span');
+        span.innerHTML = '\u00d7';
+        li.appendChild(span);
     }
     inputBox.value = '';
 }
+
+listContainer.addEventListener('click', function(e) {
+    if(e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+    } else if(e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+    }
+}, false)
