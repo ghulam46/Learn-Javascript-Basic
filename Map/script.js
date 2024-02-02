fb6cadbd169b166bec84a976eeb9fac50620bb51
Ghulam Ammar Yanuar <65816301+ghulam46@users.noreply.github.com>
2024-02-02 14:20:13 +0700

    [keyed collections] map() with condition

--- Map/script.js
+++ Map/script.js
@@ -1,5 +1,12 @@
 // References of Documentation https://thevalleyofcode.com/js-array-functions/1-map
 // map()
 const a = ["satu", "dua", "tiga"];
+const d = [1,2,3,4,5,6,7,8,9,10];
 const b = a.map(angka => console.log(angka));
 const c = a.map((angkaA, angkaB, angkaC) => console.log(angkaA, angkaB, angkaC))
+
+const filterNumbers = d.map((angkaFilter, index) => {
+    if(index > 3) {
+        console.log(angkaFilter); // result : 5,6,7,8,9,10
+    }
+}); 
