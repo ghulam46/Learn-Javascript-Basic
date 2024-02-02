b839a93d8c848e8b6ff73c548849d248c292f18a
Ghulam Ammar Yanuar <65816301+ghulam46@users.noreply.github.com>
2024-02-02 10:57:44 +0700

    [keyed collections] map()

--- Map/script.js
+++ Map/script.js
@@ -1,3 +1,5 @@
+// References of Documentation https://thevalleyofcode.com/js-array-functions/1-map
 // map()
 const a = ["satu", "dua", "tiga"];
 const b = a.map(angka => console.log(angka));
+const c = a.map((angkaA, angkaB, angkaC) => console.log(angkaA, angkaB, angkaC))
